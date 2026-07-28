d19dc9c0251ff4d7ad2b47ff2e054131806b3e97
DonatasNoreika <donoras@gmail.com>
2026-07-28 18:47:42 +0300

    Aprašyti Genre, Author modeliai, registruoti admin.py

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField()
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    first_name = models.CharField()
+    last_name = models.CharField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
