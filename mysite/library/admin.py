0d8e1c317e1b0ac1430a41ac61885f9ac201292f
DonatasNoreika <donoras@gmail.com>
2026-07-30 19:00:28 +0300

    3: Nustatytas egzempliorių sąrašas, filtras, forma

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -4,8 +4,17 @@ from .models import Genre, Author, Book, BookInstance
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'isbn', 'author', 'display_genre']
 
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ['uuid', 'book', 'status', 'due_back']
+    list_filter = ['status', 'due_back']
+
+    fieldsets = [
+        ('General', {'fields': ('uuid', 'book')}),
+        ('Availability', {'fields': ('status', 'due_back')}),
+    ]
+
 # Register your models here.
 admin.site.register(Genre)
 admin.site.register(Author)
 admin.site.register(Book, BookAdmin)
-admin.site.register(BookInstance)
+admin.site.register(BookInstance, BookInstanceAdmin)
