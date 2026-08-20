b2a11012d85421a12eacb50574241cf0e928c84e
DonatasNoreika <donoras@gmail.com>
2026-08-20 18:41:25 +0300

    10: +Sukurtas profilio modelis su photo lauku

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Genre, Author, Book, BookInstance, BookReview
+from .models import (Genre,
+                     Author,
+                     Book,
+                     BookInstance,
+                     BookReview,
+                     Profile)
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'display_books']
@@ -35,3 +40,4 @@ admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book, BookAdmin)
 admin.site.register(BookInstance, BookInstanceAdmin)
 admin.site.register(BookReview, BookReviewAdmin)
+admin.site.register(Profile)
